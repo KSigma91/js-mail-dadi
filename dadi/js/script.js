e0f306137073f7b4fbce639b6bc9f22502cc1a3e
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-09 16:14:34 +0200

    setup mail

---
+++ dadi/js/script.js
@@ -0,0 +1,3 @@
+const myNumber = document.getElementById ("mio_id");
+
+let dado = [1, 2, 3, 4, 5, 6];
---
+++ mail/js/script.js
@@ -0,0 +1,8 @@
+// chiedo all'utente la sua email e di scriverlo nel campo apposito
+const form = document.getElementById("mio_id");
+
+const myMail = prompt ("Inserisci la tua Mail");
+
+// lista degli utenti che possono accedere
+let myUser = [Pinco, Pallo, Sempronio, Antani];
+
