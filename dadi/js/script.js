f533f843343f763e10a845ca7f17419971e49693
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-10 15:46:07 +0200

    dadi completo

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -8,24 +8,30 @@ btn.addEventListener ('click',
         const myNut = document.querySelector("#mio_id");
 
         // il giocatore otterrà un numero random
-        let playerChoice = Math.floor(Math.random() * 6) + 1; 
+        const playerChoice = Math.floor(Math.random() * 6) + 1; 
 
-        console.log("playerChoice",playerChoice);
+        // stampo il numero generato dal giocatore 1
+        document.querySelector("#player_1").innerHTML = "Punteggio giocatore 1: " + playerChoice;
+
+        // console.log("playerChoice",playerChoice);
 
         // il computer otterrà un numero random
-        let machineChoice = Math.floor(Math.random() * 6) + 1;
+        const machineChoice = Math.floor(Math.random() * 6) + 1;
 
-        console.log("machineChoice",machineChoice);
+        // stampo il numero generato dal giocatore 2
+        document.querySelector("#player_2").innerHTML = "Punteggio giocatore 2: " + machineChoice;
 
-        // qui vengono confrontati i risultati
+        // console.log("machineChoice",machineChoice);
+
+        // qui vengono confrontati i risultati e stampato il risultato finale di ogni partita
         if (playerChoice > machineChoice) {
-            risultato = "Ha vinto l'utente";
+            document.querySelector("#result").innerHTML = "Ha vinto il giocatore 1";
+        } else if (playerChoice === machineChoice) {
+            document.querySelector("#result").innerHTML = "Pareggio";
         } else {
-            machineChoice > playerChoice;
-            risultato = "Ha vinto la macchina";
+            document.querySelector("#result").innerHTML = "Ha vinto il giocatore 2";
         }
 
     }
 );
 
-const result = document.querySelector(".risultato > h2").innerHTML = "Il risultato è: ";
